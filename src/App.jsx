304f1c13dcde65ac2e82e273975e48c4760a56ea
Javi <belikemike3057@hotmail.com>
2024-04-24 17:31:59 +0200

    Implementación del contador con Redux toolkit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { increment, decrement, incrementByTwo } from './store/slices/counter'
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const { counter } = useSelector(state => state.counter)
+  const dispatch = useDispatch()
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <p>count is {counter}</p>
+        <button onClick={() => dispatch(increment())}>
+          Increment
+        </button>
+        <button onClick={() => dispatch(decrement())}>
+          Decrement
+        </button>
+        <button onClick={() => dispatch(incrementByTwo())}>
+          Increment by 2
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
